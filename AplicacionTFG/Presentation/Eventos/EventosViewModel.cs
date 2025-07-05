e10ce8a5f1b3cd935fa91112b02086c655fd31c5
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2025-07-05 19:41:53 +0200

    Corregida la notificacion de que no se han rellenado todos los campos del detalle de la tarea y se ha eliminado el tipo de evento reunion

--- AplicacionTFG/Presentation/Eventos/EventosViewModel.cs
+++ AplicacionTFG/Presentation/Eventos/EventosViewModel.cs
@@ -311,7 +311,7 @@ public class EventosMesViewModel : ViewModelBase
 
     protected override void CargarPalabras()
     {
-        Tipos = new (){ _localizer["Todos"], _localizer["Reunion"], _localizer["Tarea"], _localizer["Recordatorio"] };
+        Tipos = new (){ _localizer["Todos"], _localizer["Tarea"], _localizer["Recordatorio"] };
         var lunes = new DateOnly(2023, 1, 2);
         var martes = lunes.AddDays(1);
         var miercoles = lunes.AddDays(2);
@@ -641,10 +641,10 @@ public class EventoViewModel : ViewModelBase
     {
         Evento = JsonSerializer.Deserialize(result, EventoContext.Default.Evento)!;
        
-        List<string> Tipos = new() { _localizer["Reunion"], _localizer["Tarea"], _localizer["Recordatorio"] };
+        List<string> Tipos = new() { _localizer["Tarea"], _localizer["Recordatorio"] };
         Tipo = Tipos[Evento.Tipo];
         OnPropertyChanged(nameof(Tipo));
-        if (Evento.Tipo == 1)
+        if (Evento.Tipo == 0)
         {
             Finalizada = Evento.TareaDetalle!.Finalizada;
             DetalleVisibility = Visibility.Visible;
@@ -985,7 +985,7 @@ public partial class AñadirEventoViewModel : ViewModelBase
             }
         }
     }
-    private int tipoSeleccionado = 1;
+    private int tipoSeleccionado = 0;
     private string nombre = null!;
     private string descripcion = null!;
     private string ubicacion = null!;
@@ -995,7 +995,7 @@ public partial class AñadirEventoViewModel : ViewModelBase
     public DateTimeOffset? DiaFin { get; set; } = null;
     public TimeSpan? HoraFin { get; set; } = null;
     public List<string> Tipos { get; set; } = null!;
-    public int TipoSeleccionado { get => tipoSeleccionado; set { tipoSeleccionado = value; TareaVisibility = value == 1 ? Visibility.Visible : Visibility.Collapsed; OnPropertyChanged(nameof(TipoSeleccionado)); } }
+    public int TipoSeleccionado { get => tipoSeleccionado; set { tipoSeleccionado = value; TareaVisibility = value == 0 ? Visibility.Visible : Visibility.Collapsed; OnPropertyChanged(nameof(TipoSeleccionado)); } }
     public int Cantidad { get => cantidad; set => cantidad = value; }
     public string Unidad { get => unidad; set => unidad = value; }
     public List<UsuarioNombre> Empleados { get; set; } = null!;
@@ -1075,7 +1075,7 @@ public partial class AñadirEventoViewModel : ViewModelBase
 #endif
         try
         {
-            if (TipoSeleccionado == 1 && (Unidad is null || Cantidad == 0))
+            if (TipoSeleccionado == 0 && (Unidad is null || Cantidad == 0))
             {
                 var acciones = new[]
                 {
@@ -1084,8 +1084,9 @@ public partial class AñadirEventoViewModel : ViewModelBase
                 };
                 string texto = _localizer["DetallesTareaAlerta"];
                 string[] parts = texto.Split('\\');
-                string textoseparado = parts[0] + "\n" + parts[1] + "\n" + parts[2];
-                string resultado = await _navigator.ShowMessageDialogAsync<string>(this, title: _localizer["Error"], content: textoseparado, buttons: acciones);
+                var textoUnidad = Unidad ?? "nan";
+                string textoseparado = parts[0] + "\n" + parts[1] +" "+ Cantidad + "\n" + parts[2] +" "+textoUnidad;
+                string? resultado = await _navigator.ShowMessageDialogAsync<string>(this, title: _localizer["Error"], content: textoseparado, buttons: acciones);
                 if (resultado != _localizer["Aceptar"].ToString())
                     return;
             }
@@ -1094,16 +1095,16 @@ public partial class AñadirEventoViewModel : ViewModelBase
                 Nombre = Nombre,
                 Descripcion = Descripcion,
                 Ubicacion = Ubicacion,
-                Color = "#FF0B83E6",
+                Color = TipoSeleccionado ==0 ?"#FF0B83E6": "#FFFFFFFF",
                 Inicio = DiaInicio.Date + HoraInicio,
                 Fin = DiaFin.HasValue ? DiaInicio.Date + HoraInicio : null,
                 Tipo = TipoSeleccionado,
                 UsuarioId = EmpleadoSeleccionado is null ? _usuarioId : EmpleadoSeleccionado.Id,
                 EmpresaId = Usuario.EmpresaId,
-                TareaDetalle = TipoSeleccionado == 1 ? new TareaDetalleDto
+                TareaDetalle = TipoSeleccionado == 0 ? new TareaDetalleDto
                 {
                     Cantidad = Cantidad,
-                    Unidad = Unidad ?? "Unidades"
+                    Unidad = Unidad ?? "nan"
                 } : null
             };
             if (!ValidarModelo(Evento))
@@ -1129,7 +1130,7 @@ public partial class AñadirEventoViewModel : ViewModelBase
 
     protected override void CargarPalabras()
     {
-        Tipos = new() { _localizer["Reunion"], _localizer["Tarea"], _localizer["Recordatorio"] };
+        Tipos = new() { _localizer["Tarea"], _localizer["Recordatorio"] };
         Nombre_Loc = _localizer["Nombre"];
         Descripcion_Loc = _localizer["Descripcion"];
         Ubicacion_Loc = _localizer["Ubicacion"];
